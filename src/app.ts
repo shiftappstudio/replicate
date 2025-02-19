4f74b520a6e6e91fe8ade47a938a461df49885bf
shiftmgrepo <159116150+shiftmgrepo@users.noreply.github.com>
2025-02-19 18:32:49 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ import { FirebaseRoutes } from "./routes/firebase.routes";
 const app = express();
 app.use(cors({ origin: true }));
 
-// Serve static files from the 'public' directory in the root
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(bodyParser.json());
